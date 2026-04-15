47806fda8c8a090a73b61a30b80e7cc12fee286a
vincent timperio <vincent.timperio@gmail.com>
2026-04-15 10:29:43 +0200

    Year month and day initiated.

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -15,7 +15,10 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data together!')
+    city = None
+    month = None
+    day = None
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 
